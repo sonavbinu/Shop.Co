4325fc69806f42c6621c4af78e6e259573609d35
sonavbinu <sonavbinu567@gmail.com>
2025-09-21 21:26:01 +0530

    setting up navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.scss';
+import Navbar from './Components/Navbar/Navbar';
 
 const App: React.FC = () => {
   return (
-    <div className="signup-banner">
-      <h5>Sign up and get 20% off to your first order. </h5>
-      <Link to="/signup">Sign Up now</Link>
+    <div>
+      <div className="signup-banner">
+        <h5>Sign up and get 20% off to your first order. </h5>
+        <Link to="/signup">Sign Up now</Link>
+      </div>
+      <div>
+        <Navbar />
+      </div>
     </div>
   );
 };
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import './Navbar.scss';
 
 const Navbar: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="navbar-main">
+      <img src="/SHOP.CO.png" alt="" />
+      <div>
+        <span>
+          Shop <span>v</span>{' '}
+        </span>
+        <span>On Sale</span>
+        <span>New Arrivals</span>
+        <span>Brands</span>
+      </div>
+      <div>
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder="Search for products..." />
+      </div>
+      <div>
+        <i className="fa-solid fa-cart-shopping"></i>
+        <i className="fa-solid fa-user"></i>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
