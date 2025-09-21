87e37a1e6b7439e8fe487c8683b5a7951958784c
sonavbinu <sonavbinu567@gmail.com>
2025-09-21 22:05:51 +0530

    navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -5,19 +5,25 @@ const Navbar: React.FC = () => {
   return (
     <div className="navbar-main">
       <img src="/SHOP.CO.png" alt="" />
-      <div>
-        <span>
-          Shop <span>v</span>{' '}
+      <div className="primary-menu">
+        <span className="shop-dropmenu">
+          Shop{' '}
+          <span>
+            <img src="/drop.png" alt="" />
+          </span>{' '}
         </span>
         <span>On Sale</span>
         <span>New Arrivals</span>
         <span>Brands</span>
       </div>
-      <div>
-        <i className="fa-solid fa-magnifying-glass"></i>
-        <input type="text" placeholder="Search for products..." />
+      <div className="input-menu">
+        <div className="input-wrapper">
+          {' '}
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input type="text" placeholder="Search for products..." />
+        </div>
       </div>
-      <div>
+      <div className="usercart-icon">
         <i className="fa-solid fa-cart-shopping"></i>
         <i className="fa-solid fa-user"></i>
       </div>
